d7b8c1384e9a9bc89e98ba88ffa1caa70afef87e
nascbiel <bielnascsantos@gmail.com>
2021-01-26 11:34:43 -0300

    Atividade Farmacia CRUD

---
+++ farmacia/src/main/java/com/farmacia/farmacia/model/Produto.java
@@ -0,0 +1,75 @@
+package com.farmacia.farmacia.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "produto")
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min=2 , max = 500)
+	private String nomeDoProduto;
+	
+	@NotNull
+	@Size(min=2 , max = 1000)
+	private String informacoes;
+	
+	@NotNull
+	private double preco;
+	
+	@NotNull
+	private long quantidade;
+
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNomeDoProduto() {
+		return nomeDoProduto;
+	}
+
+	public void setNomeDoProduto(String nomeDoProduto) {
+		this.nomeDoProduto = nomeDoProduto;
+	}
+
+	public String getInformacoes() {
+		return informacoes;
+	}
+
+	public void setInformacoes(String informacoes) {
+		this.informacoes = informacoes;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public long getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(long quantidade) {
+		this.quantidade = quantidade;
+	}
+	
+	
+}
