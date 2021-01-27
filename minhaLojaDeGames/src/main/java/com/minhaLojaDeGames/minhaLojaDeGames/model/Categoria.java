bb68ea4ba6de3264722e4fc9194f14ea854fef60
nascbiel <bielnascsantos@gmail.com>
2021-01-27 13:21:12 -0300

    Relação entre tabelas Spring lojaDeGames

---
+++ minhaLojaDeGames/src/main/java/com/minhaLojaDeGames/minhaLojaDeGames/model/Categoria.java
@@ -0,0 +1,76 @@
+package com.minhaLojaDeGames.minhaLojaDeGames.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.sun.istack.NotNull;
+
+@Entity
+@Table(name = "categoria")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min=2,max=500)
+	private String categoria;
+	
+	@NotNull
+	@Size(min=2,max=500)
+	private String plataforma;
+	
+	@OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL) 
+	@JsonIgnoreProperties("categoria")
+	private List<Produto> produto;
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+	public String getPlataforma() {
+		return plataforma;
+	}
+
+	public void setPlataforma(String plataforma) {
+		this.plataforma = plataforma;
+	}
+
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
+	}
+
+
+
+	
+
+	
+
+	
+}
---
+++ minhaLojaDeGames/src/main/java/com/minhaLojaDeGames/minhaLojaDeGames/model/Produto.java
@@ -0,0 +1,90 @@
+package com.minhaLojaDeGames.minhaLojaDeGames.model;
+
+
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.sun.istack.NotNull;
+
+@Entity
+@Table(name = "produto")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min=2,max=500)
+	private String nomeProduto;
+	
+	@NotNull
+	@Size(min=2,max=1000)
+	private String descricao;
+	
+	@NotNull
+	private double preco;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+	
+	/* OneToMany
+	 * ManyToOne
+	 */
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNomeProduto() {
+		return nomeProduto;
+	}
+
+	public void setNomeProduto(String nomeProduto) {
+		this.nomeProduto = nomeProduto;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	
+
+	
+
+	
+	
+}
